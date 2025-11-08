ec49648514b9cfa2e664592b392e5552bd776a8b
greek-n-chic <moisovadiana@gmail.com>
2025-11-08 19:18:03 +0300

    grrgrg

--- gendiff.js
+++ gendiff.js
@@ -11,14 +11,14 @@ program
   .version('output the version number')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const absolutePath1 = path.resolve(filepath1);
-    const absolutePath2 = path.resolve(filepath2);
+    const absolutePath1 = path.resolve(filepath1)
+    const absolutePath2 = path.resolve(filepath2)
     const file1 = readFileSync(absolutePath1, 'utf-8')
     const file2 = readFileSync(absolutePath2, 'utf-8')
     const obj1 = parseFile(file1)
     const obj2 = parseFile(file2)
     const compared = compare(obj1, obj2)
-    console.log(compared);
+    console.log(compared)
   })
   .option('-f, --format [type]', 'output format')
   .parse(process.argv)
