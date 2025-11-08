617ce01f02a83314a0ff1dd5a88c5ab06cb4f74d
greek-n-chic <moisovadiana@gmail.com>
2025-11-08 19:04:41 +0300

    actions 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,5 @@
-const js = require('@eslint/js')
-const globals = require('globals')
+const js = require('@eslint/js');
 
 module.exports = [
-  js.configs.recommended,
-  {
-    files: ['**/*.js'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'commonjs',
-      globals: {
-        ...globals.node,
-        ...globals.jest
-      }
-    },
-    rules: {
-      'no-console': 'off',
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'always']
-    }
-  }
-]
+  js.configs.recommended
+];
--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-const { parseFile } = require('./parser')
-const { readFileSync } = require('node:fs')
-const path = require('path')
+const { parseFile } = require('./parser');
+const { readFileSync } = require('node:fs');
+const path = require('path');
 const { Command } = require('commander');
 const { compare } = require('./comparer');
 const program = new Command();
@@ -11,14 +11,14 @@ program
   .version('output the version number')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const absolutePath1 = path.resolve(filepath1)
-    const absolutePath2 = path.resolve(filepath2)
-    const file1 = readFileSync(absolutePath1, 'utf-8')
-    const file2 = readFileSync(absolutePath2, 'utf-8')
-    const obj1 = parseFile(file1)
-    const obj2 = parseFile(file2)
-    const compared = compare(obj1, obj2)
-    console.log(compared)
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+    const file1 = readFileSync(absolutePath1, 'utf-8');
+    const file2 = readFileSync(absolutePath2, 'utf-8');
+    const obj1 = parseFile(file1);
+    const obj2 = parseFile(file2);
+    const compared = compare(obj1, obj2);
+    console.log(compared);
   })
   .option('-f, --format [type]', 'output format')
-  .parse(process.argv)
+  .parse(process.argv);
