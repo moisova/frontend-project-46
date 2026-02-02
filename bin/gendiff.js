6e783a92169ff34a8b609a6ccd746bbb2283449a
greek-n-chic <moisovadiana@gmail.com>
2026-02-02 12:33:26 +0300

    Second step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,6 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
 
 program.parse()
